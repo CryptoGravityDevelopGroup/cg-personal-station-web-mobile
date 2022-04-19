b5aed5072e01166efb5f152b70f26903489233f1
caohuiboss <2550465923@qq.com>
2022-04-19 22:13:53 +0800

    初始化移动端

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+<script setup></script>
+
+<style lang="less">
+.footer {
+  width: 100%;
+  background: #1d0f3d;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
